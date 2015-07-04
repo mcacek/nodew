f35e882325ceabce2e1a10e9972617e525d61c07
Michael Cacek <michael.cacek@so.mnscu.edu>
2015-07-04 15:49:36 -0500

    MISC - Update messaging.

--- setup.sh
+++ setup.sh
@@ -3,9 +3,7 @@
 NODEWRC=".nodewrc"
 NVM_DIR="$HOME/.nvm"
 
-echo "$PWD/$NODEWRC"
 if test -e "$PWD/$NODEWRC"; then
-  echo "sourcing"
   source .nodewrc
 else
   NODEW_NVM_VERSION="v0.25.4"
@@ -13,16 +11,17 @@ else
 fi
 
 if [ ! -d "$NVM_DIR" ]; then
+  echo "Installing NVM..."
+
   # Download NVM if not currently installed.
   curl -o- https://raw.githubusercontent.com/creationix/nvm/$NODEW_NVM_VERSION/install.sh | bash
-  echo "Installing NVM..."
 fi
 
 # Add node to our path
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
 
 if [ ! -d "$NVM_DIR/$NODEW_NODE_VERSION" ]; then
-  echo "Installing $NODEW_NODE_VERSION."
+  echo "Installing $NODEW_NODE_VERSION..."
   nvm install $NODEW_NODE_VERSION
 fi
 
